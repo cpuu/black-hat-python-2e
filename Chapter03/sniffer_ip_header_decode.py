46ca86fdc9af0010a9b67c1039b473f11d56ea9a
cpuu <cpuu@icloud.com>
2021-06-17 12:20:36 +0900

    Update sniffer_ip_header_decode.py

--- Chapter03/sniffer_ip_header_decode.py
+++ Chapter03/sniffer_ip_header_decode.py
@@ -21,11 +21,11 @@ class IP:
         self.src = header[8]
         self.dst = header[9]
 
-        # human readable IP addresses
+        # 사람이 이해하기 쉬운 IP 주소 형태로 표기
         self.src_address = ipaddress.ip_address(self.src)
         self.dst_address = ipaddress.ip_address(self.dst)
 
-        # map protocol constants to their names
+        # 프로토콜 이름에 알맞은 고유번호를 연계하여 저장
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
         try:
             self.protocol = self.protocol_map[self.protocol_num]
@@ -35,6 +35,7 @@ class IP:
 
 
 def sniff(host):
+    # 앞선 예제의 코드 준용
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
     else:
@@ -49,13 +50,17 @@ def sniff(host):
 
     try:
         while True:
+            # 패킷 수신
             raw_buffer = sniffer.recvfrom(65535)[0]
+            # 패킷의 처음 20 바이트 부분을 추출하여 IP헤더 생성
             ip_header = IP(raw_buffer[0:20])
+            # 탐지된 프로토콜 및 호스트 주소 출력
             print('Protocol: %s %s -> %s' % (ip_header.protocol,
                                              ip_header.src_address, ip_header.dst_address))
             # print(f'Version: {ip_header.ver} Header Length: {ip_header.ihl}  TTL: {ip_header.ttl}')
 
     except KeyboardInterrupt:
+        # 윈도우 시스템의 경우, 무차별 모드 해제
         if os.name == 'nt':
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
         sys.exit()
@@ -65,5 +70,5 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         host = sys.argv[1]
     else:
-        host = '192.168.1.206'
+        host = '192.168.1.203'
     sniff(host)
