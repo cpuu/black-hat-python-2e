3ca622005532a495ce9cc9bc8bea07db0c4b4f9b
cpuu <cpuu@icloud.com>
2021-06-17 11:20:18 +0900

    add ch4 files

---
+++ Chapter04/mail_sniffer1.py
@@ -0,0 +1,13 @@
+from scapy.all import sniff
+
+
+def packet_callback(packet):
+    print(packet.show())
+
+
+def main():
+    sniff(prn=packet_callback, count=1)
+
+
+if __name__ == '__main__':
+    main()
